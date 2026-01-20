78a9b8c77b236f26ff0e3caf442dc85ba3c96337
priyanshutiwary <priyanshutiwary711@gmail.com>
2026-01-20 14:35:33 +0530

    removed yarn

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
-  // Pass environment variables to the worker
-  // This ensures .dev.vars are available in preview mode
-});
+export default defineCloudflareConfig({});
